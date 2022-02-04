76b161d5aeb8d5906f32c017204338797112841d
Cristian Ceballos <cristian.ceballos@masglobalconsulting.com>
2022-02-04 10:11:21 -0500

    update

--- ClientApp/src/commons/core/clientComponents/AddNewClient.js
+++ ClientApp/src/commons/core/clientComponents/AddNewClient.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Grid from "@material-ui/core/Grid";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
-import { Box, Checkbox, FormControlLabel } from '@material-ui/core';
+import {Checkbox, FormControlLabel } from '@material-ui/core';
 
 import '../../../assets/css/addNewClient.css';
 
